6638d934cd203cbed51803289c73157847a5c8c7
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-08-10 00:37:33 -0400

    Update and rename IReflections.sol to IReflect.sol

--- contracts/IReflect.sol
+++ contracts/IReflect.sol
@@ -1,7 +1,7 @@
 
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-interface IReflections {
+interface IReflect {
     function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external;
     function setShare(address payable shareholder, uint256 amount) external;
     function deposit() external payable;
